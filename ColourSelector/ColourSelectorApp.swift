505f1be05c75beec46f5491c00147ca739ff1004
Russell Gordon <github@russellgordon.ca>
2022-10-27 14:40:44 -0400

    Initial Commit

---
+++ ColourSelector/ColourSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColourSelectorApp.swift
+//  ColourSelector
+//
+//  Created by Russell Gordon on 2022-10-27.
+//
+
+import SwiftUI
+
+@main
+struct ColourSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
